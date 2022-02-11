c2bcf86f827db88791ec64915c6d8468ee6a4807
aakzsh <aakashferrari@gmail.com>
2022-02-11 22:00:23 +0530

    add files

---
+++ lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        decoration: const BoxDecoration(color: Colors.pink),
+      ),
+    );
+  }
+}
