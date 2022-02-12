fb6ed62da5debc082fc5515b2f56698f3d530127
shruti <shrutigupta5555@gmail.com>
2022-02-12 18:02:54 +0530

    home page ui

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,8 +16,51 @@ class _HomeState extends State<Home> {
       body: Container(
         height: height,
         width: width,
-        decoration: const BoxDecoration(color: Colors.pink),
-        child: const Center(child: Text("hemlo")),
+        decoration: const BoxDecoration(color: Color(0xff45C5BD)),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 16.0, 0, 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Image.asset('assets/frame.png', width: 100),
+                    CircleAvatar(
+                      radius: 35.0,
+                      backgroundImage: AssetImage('assets/avatar.png'),
+                      backgroundColor: Colors.black12,
+                    )
+                  ],
+                ),
+              ),
+              Image.asset('assets/desk.png', width: width - 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: CircleBorder(),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.lightbulb_outline,
+                        color: Colors.white,
+                        size: 40,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
