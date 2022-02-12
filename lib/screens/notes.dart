bf6043b72aba782aeb7ee228d41d1a29ef85188d
shruti <shrutigupta5555@gmail.com>
2022-02-13 01:16:02 +0530

    page layout

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Notes extends StatefulWidget {
   const Notes({Key? key}) : super(key: key);
@@ -10,15 +11,33 @@ class Notes extends StatefulWidget {
 class _NotesState extends State<Notes> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     final PageController controller = PageController();
     return Scaffold(
       body: PageView(
         /// [PageView.scrollDirection] defaults to [Axis.horizontal].
         /// Use [Axis.vertical] to scroll vertically.
         controller: controller,
-        children: const <Widget>[
+        children: <Widget>[
           Center(
-            child: Text('First Page'),
+            child: Container(
+                height: height,
+                width: width,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/page.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Center(
+                    child: Text("man its been weird bjnikjkol",
+                        style: GoogleFonts.shadowsIntoLight(
+                            textStyle: Theme.of(context).textTheme.headline4)),
+                  ),
+                )),
           ),
           Center(
             child: Text('Second Page'),
