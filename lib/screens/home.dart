a5fab705e1cdcb7b3611ac27713312c6d20f03a9
shruti <shrutigupta5555@gmail.com>
2022-02-12 19:04:00 +0530

    change bg color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,16 +8,25 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+Color currentColor = Color(0xff45C5BD);
+
 class _HomeState extends State<Home> {
+  bool _enableLabel = true;
+  bool _portraitOnly = false;
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
+
+    void changeColor(Color color) => setState(() {
+          currentColor = color;
+        });
     return Scaffold(
+      // create some values
       body: Container(
         height: height,
         width: width,
-        decoration: const BoxDecoration(color: Color(0xff45C5BD)),
+        decoration: BoxDecoration(color: currentColor),
         child: Padding(
           padding: const EdgeInsets.all(32.0),
           child: Column(
@@ -46,15 +56,32 @@ class _HomeState extends State<Home> {
                       backgroundColor: Colors.black,
                       shape: CircleBorder(),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
                       child: Icon(
                         Icons.lightbulb_outline,
                         color: Colors.white,
                         size: 40,
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            titlePadding: const EdgeInsets.all(0),
+                            contentPadding: const EdgeInsets.all(0),
+                            content: SingleChildScrollView(
+                              child: ColorPicker(
+                                pickerColor: currentColor,
+                                onColorChanged: changeColor,
+                                portraitOnly: _portraitOnly,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
                   ),
                 ],
               )
